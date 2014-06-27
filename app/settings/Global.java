cadc1c794a1b97c386071130b03961c315bb2ef2
Knut Liestoel <knut_boerte@live.no>
2014-06-27 23:41:58 +0200

    Added allow-headers header in all responses. Once again trying to get CORS to work

--- app/settings/Global.java
+++ app/settings/Global.java
@@ -118,6 +118,7 @@ public class Global extends GlobalSettings {
             response.setHeader("Access-Control-Allow-Origin", "*");
             response.setHeader("Allow", "*");
             response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+            response.setHeader("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept, Referer, User-Agent");
             
             return result;
         }
